556f651326a7f83c569389125f8f2a26b79bf6a4
SachinSharma-LordHeydes <thereisamailforme@gmail.com>
2025-11-02 15:47:08 +0545

    search ui added

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,28 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com", // ✅ Added Unsplash
-      },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  experimental: {
+    // TypeScript will ignore unknown keys if not typed explicitly
+    outputFileTracingExcludes: {
+      "*": [
+        "node_modules/@swc/core-linux-x64-gnu",
+        "node_modules/@swc/core-linux-x64-musl",
+        "node_modules/@esbuild/linux-x64",
+      ],
+    },
+  },
+  webpack: (config:any, { isServer }:{isServer:any}) => {
+    if (isServer) {
+      config.externals.push("@prisma/client");
+    }
+    return config;
   },
 };
 
