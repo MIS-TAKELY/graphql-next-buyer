a138f0068f04b6c3197a28d1ad20cfde3a55ab8e
SachinSharma-LordHeydes <thereisamailforme@gmail.com>
2025-11-02 16:56:17 +0545

    few changes on embedding

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,43 +1,11 @@
-// import { createContext } from "@/servers/gql/context";
-// import { createYoga } from "graphql-yoga";
-// import type { NextRequest } from "next/server";
-// import { schema } from "../../../servers/gql/index";
-
-// const yoga = createYoga<{
-//   req: NextRequest;
-// }>({
-//   schema,
-//   graphqlEndpoint: "/api/graphql",
-//   fetchAPI: { Request, Response },
-//   context: async ({ request }: { request: NextRequest }) => {
-//     return await createContext(request);
-//   },
-//   cors: {
-//     origin:
-//       process.env.NODE_ENV === "production"
-//         ? [
-//             "https://graphql-next-buyer-hmu9c58z1-mailitttome-4974s-projects.vercel.app", // frontend
-//             "https://graphql-next-buyer.vercel.app", // API itself (optional, in case of same-origin requests)
-//           ]
-//         : ["http://localhost:3000"],
-//     credentials: true,
-//   },
-// });
-
-// export async function GET(request: NextRequest) {
-//   return yoga.handleRequest(request, { req: request });
-// }
-
-// export async function POST(request: NextRequest) {
-//   return yoga.handleRequest(request, { req: request });
-// }
-
 import { createContext } from "@/servers/gql/context";
 import { createYoga } from "graphql-yoga";
 import type { NextRequest } from "next/server";
 import { schema } from "../../../servers/gql/index";
 
-const yoga = createYoga<{ req: NextRequest }>({
+const yoga = createYoga<{
+  req: NextRequest;
+}>({
   schema,
   graphqlEndpoint: "/api/graphql",
   fetchAPI: { Request, Response },
@@ -48,8 +16,8 @@ const yoga = createYoga<{ req: NextRequest }>({
     origin:
       process.env.NODE_ENV === "production"
         ? [
-            "https://graphql-next-buyer-hmu9c58z1-mailitttome-4974s-projects.vercel.app",
-            "https://graphql-next-buyer.vercel.app",
+            "https://graphql-next-buyer-hmu9c58z1-mailitttome-4974s-projects.vercel.app", // frontend
+            "https://graphql-next-buyer.vercel.app", // API itself (optional, in case of same-origin requests)
           ]
         : ["http://localhost:3000"],
     credentials: true,
@@ -64,7 +32,3 @@ export async function POST(request: NextRequest) {
   return yoga.handleRequest(request, { req: request });
 }
 
-// IMPORTANT: handle preflight
-export async function OPTIONS(request: NextRequest) {
-  return yoga.handleRequest(request, { req: request });
-}
