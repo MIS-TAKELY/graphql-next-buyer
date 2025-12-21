74f53796a3b4840b2a705c39032e2a48377e1a45
MISTAKELY <thereisamailforme@gmail.com>
2025-12-21 20:38:07 +0545

    added new logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -9,7 +9,7 @@ const Logo = () => (
         alt="Logo"
         width={90}
         height={90}
-        className="filter hover:cursor-pointer w-[50px] h-[50px] xs:w-[60px] xs:h-[60px] sm:w-[70px] sm:h-[70px] md:w-[80px] md:h-[80px] lg:w-[90px] lg:h-[90px]"
+        className="filter hover:cursor-pointer w-[50px] h-[50px] xs:w-[60px] xs:h-[60px] sm:w-[70px] sm:h-[70px] md:w-[80px] md:h-[80px] lg:w-[90px] lg:h-[90px] dark:invert dark:brightness-0 dark:contrast-100 "
         priority
       />
     </Link>
@@ -19,7 +19,7 @@ const Logo = () => (
         alt="Logo"
         width={32}
         height={32}
-        className="filter hover:cursor-pointer "
+        className="filter hover:cursor-pointer dark:invert dark:brightness-0 dark:contrast-100"
         priority
       />
     </Link>
