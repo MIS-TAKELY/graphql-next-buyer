bbd2f36894408c00914603d7aaea9e61ba1997fc
MISTAKELY <thereisamailforme@gmail.com>
2026-01-06 14:23:39 +0545

    orisma version

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,10 @@
 // prisma.config.ts
-import 'dotenv/config';
-import { defineConfig, env } from '@prisma/config';
-
-export default defineConfig({
-  schema: 'prisma/schema.prisma',           // or './prisma/schema.prisma' if needed
-
+export default {
+  schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
   },
-
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL,
   },
-});
+};
