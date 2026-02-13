136675e3816dc52a94b641f388c45171186ba234
MISTAKELY <thereisamailforme@gmail.com>
2026-02-13 20:58:17 +0545

    phone number and password login

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -12,7 +12,7 @@ export const authClient = createAuthClient({
             id: "phone-password",
             getActions: (client) => ({
                 signInPhone: async (data: { phone: string, password: string, rememberMe?: boolean }) => {
-                    return client("/phone-login", {
+                    return client("/phone-password/phone-login", {
                         method: "POST",
                         body: data
                     })
