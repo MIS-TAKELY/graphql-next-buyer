a1229a738f80dab17d2fd9ead6fa1d604b0d3e45
MISTAKELY <thereisamailforme@gmail.com>
2026-02-22 22:50:55 +0545

    fixed:duplicate best deals

--- client/landing/topdeals.query.ts
+++ client/landing/topdeals.query.ts
@@ -1,14 +1,52 @@
-import { PRODUCT_CARD_FRAGMENT } from "../product/product.queries";
 import gql from "graphql-tag";
 
 export const GET_TOP_DEALS = gql`
   query GetTopDealSaveUpTo($topDealAbout: String!, $limit: Int!) {
     getTopDealSaveUpTo(topDealAbout: $topDealAbout, limit: $limit) {
-      ...ProductCardFields
+      id
+      name
+      slug
+      brand
+      status
+      description
+      createdAt
       saveUpTo
       imageUrl
       imageAltText
+      images {
+        id
+        url
+        altText
+        mediaType
+      }
+      variants {
+        id
+        price
+        mrp
+        stock
+        isDefault
+        attributes
+        specifications {
+          key
+          value
+        }
+      }
+      category {
+        id
+        name
+      }
+      reviews {
+        id
+        rating
+      }
+      productOffers {
+        id
+        offer {
+          type
+          value
+          isActive
+        }
+      }
     }
   }
-  ${PRODUCT_CARD_FRAGMENT}
 `;
